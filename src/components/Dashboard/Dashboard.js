73e40335c0b3db49ab975b0bf293254c22e41591
guilherme-braga4 <bragaguilherme152@yahoo.com.br>
2022-02-18 16:41:57 -0300

    ADD new Asset onClick

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,48 +3,65 @@ import { Container } from "./styled";
 import { ContainerInput } from "./styled";
 
 const Dashboard = () => {
-  const [form, setForm] = useState({});
+  const [form, setForm] = useState([]);
+  const [asset, setAsset] = useState([]);
   console.log("form", form);
 
-  const object = {
-    nome: form?.nome,
+  const handleAddAsset = (event) => {
+    const assetsList = Array.from(asset);
+    console.log("assetList", assetsList);
+    assetsList.push({ form });
+    setAsset(assetsList);
+    console.log("asset", asset);
   };
 
-  console.log("object", object);
-  console.log("form.nome", form?.nome);
-
-  async function handleSubmit() {
-    console.log("aqui");
+  async function handleSubmit(event) {
+    event.preventDefault();
+    console.log("submit");
   }
 
   const handleChange = (event) => {
     console.log("event.target.value", event.target.value);
-    setForm({ [event.target.name]: event.target.value });
-    // console.log("event.target", event.target);
-    // console.log("event", event);
+    setForm({ ...form, [event.target.name]: event.target.value });
+    console.log("handleChange", form);
   };
 
   return (
     <Container>
       <form onSubmit={handleSubmit}>
         <ContainerInput>
-          <label>Ativo</label>
-          <input
-            name="nome"
-            onChange={handleChange}
-            placeholder="Digite o Nome do Ativo"
-            value={form?.ativo}
-          />
-          <button>Adicionar</button>
-          <label>Quantidade</label>
-          <input
-            type="number"
-            name="nome"
-            onChange={handleChange}
-            placeholder="Digite o Valor Investido"
-            value={form?.quantidade}
-          />
-          <button>Adicionar</button>
+          <div>
+            <label>Ativo</label>
+            <input
+              name="nome"
+              onChange={handleChange}
+              placeholder="Digite o Nome do Ativo"
+            />
+            <label>Quantidade</label>
+
+            <input
+              type="number"
+              name="quantidade"
+              onChange={handleChange}
+              placeholder="Digite o Valor Investido"
+            />
+          </div>
+          <button
+            type="submit"
+            onClick={() => {
+              handleAddAsset();
+            }}
+          >
+            Adicionar
+          </button>
+          {asset.map((asset) => {
+            return (
+              <>
+                <div>{asset?.form?.nome}</div>
+                <div>R$ {asset?.form?.quantidade}</div>
+              </>
+            );
+          })}
         </ContainerInput>
       </form>
     </Container>
