537f1fb09ddb7a54cf6e93f81f07f83540669e8f
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2022-05-22 09:47:55 -0300

    config: express server

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
-console.log("serve.js")
+const express = require('express');
+const path = require('path');
 
-// "start": "node server.js",
+const app = express();
+
+//Estáticos -> Roteamento dos Componentes
+ app.use(express.static(path.join(__dirname, 'build')));
+
+//  app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// })
+
+app.listen(process.env.PORT || 3000, (err) => {
+  err ? console.log("err", err) : console.log("Tudo Funcionando!")
+});
