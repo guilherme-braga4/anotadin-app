5e651ca5fbf455c979a0f9eeb59db2aa2f23ddf2
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2022-05-22 11:19:11 -0300

    fix: server

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const app = express();
 //Estáticos -> Roteamento dos Componentes
  app.use(express.static(path.join(__dirname, 'build')));
 
-//  app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// })
+ app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname + '/client/build/index.html'))
+})
 
 app.listen(process.env.PORT || 3000, (err) => {
   err ? console.log("err", err) : console.log("Tudo Funcionando!")
