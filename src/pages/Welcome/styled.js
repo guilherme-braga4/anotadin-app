631aeba9b31c43ab298cc3316d9f2d89c90d31e5
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2022-05-22 11:09:09 -0300

    welcome page add

---
+++ src/pages/Welcome/styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+`
+
+export const ContainerInfos = styled.div`
+  background-color: #24252d;
+  height: 100%;
+
+  
+`
