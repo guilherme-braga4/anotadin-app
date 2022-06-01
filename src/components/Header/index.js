1492f4176a7dc030ab30bd7e0d007abad841a491
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2022-06-01 19:40:17 -0300

    change Routes on Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {
   HeaderStyled,
   ContainerStyled,
@@ -14,12 +14,13 @@ import { useNavigate } from "react-router-dom";
 
 
 const Header = () => {
-  const { data } = useContext(AuthContext)
+  const { data, rendimentos, setRendimentos } = useContext(AuthContext)
   console.log("data Header", data)
   const navigate = useNavigate()
   const userName = localStorage.getItem("@AnotadinApp User_Nome")
+  console.log("rendimentos", rendimentos)
   
-
+  if (rendimentos == false) {
   return (
     <>
       <Container>
@@ -35,7 +36,9 @@ const Header = () => {
               <a>Estruturando a sua liberdade financeira!</a>
             </div>
             <ContainerButton>
-              <ButtonFilled>Rendimentos</ButtonFilled>
+              <ButtonFilled onClick={() => 
+                 {setRendimentos(true)
+                 navigate('/rendimentos')}}>Rendimentos</ButtonFilled>
               <ButtonFilled>Perfil</ButtonFilled>
               <ButtonNoBackground onClick={() => {
                 navigate("/login", { replace: true }) 
@@ -49,6 +52,40 @@ const Header = () => {
       </Container>
     </>
   );
+}
+else {
+  return (
+    <>
+      <Container>
+        <HeaderUser>
+          <h1>Seja bem Vindo</h1>
+          <a>{userName}</a>
+        </HeaderUser>
+        <HeaderStyled>
+          <ContainerStyled>
+            <div>
+              <img src={logo} alt="logo anotadin"></img>
+              <h1>Anotadin</h1>
+              <a>Estruturando a sua liberdade financeira!</a>
+            </div>
+            <ContainerButton>
+              <ButtonFilled onClick={() => 
+                 {setRendimentos(false)
+                 navigate('/dashboard')}}>Dashboard</ButtonFilled>
+              <ButtonFilled>Perfil</ButtonFilled>
+              <ButtonNoBackground onClick={() => {
+                navigate("/login", { replace: true }) 
+                localStorage.removeItem("@AnotadinApp Auth")
+                localStorage.removeItem("@AnotadinApp JWT")
+                window.location.reload();
+                }}>Sair</ButtonNoBackground>
+            </ContainerButton>
+          </ContainerStyled>
+        </HeaderStyled>
+      </Container>
+    </>
+  );
+}
 };
 
 export default Header;
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -3,11 +3,15 @@ import React, {useState, createContext} from 'react'
 export const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
-  const [data, setData] = useState([])
-  const [auth, setAuth] = useState(false)
+  const authenticatedId = localStorage.getItem("@AnotadinApp User_Id")
 
+  const [data, setData] = useState({id: authenticatedId})
+  const [auth, setAuth] = useState(false)
+  const [rendimentos, setRendimentos] = useState(false)
+
+  
   return ( 
-    <AuthContext.Provider value={{auth, setAuth, data, setData}}>
+    <AuthContext.Provider value={{auth, setAuth, data, setData, rendimentos, setRendimentos}}>
       {children}
     </AuthContext.Provider>
    );
