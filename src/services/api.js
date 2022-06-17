ab279a45f12126c059d5f643a22a89a3c7e3ef01
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2022-06-17 10:20:28 -0300

    alter local API to prod API

--- src/services/api.js
+++ src/services/api.js
@@ -4,17 +4,17 @@ const token = localStorage.getItem("@AnotadinApp JWT");
 console.log("token API", token);
 
 
-const api = axios.create({
-  baseURL: 'http://127.0.0.1:3333/api', headers: {
-      Authorization: `Bearer ${token}`
-  },
-});
-
 // const api = axios.create({
 //   baseURL: 'http://127.0.0.1:3333/api', headers: {
 //       Authorization: `Bearer ${token}`
 //   },
 // });
+
+const api = axios.create({
+  baseURL: 'https://anotadin-api.herokuapp.com/api', headers: {
+      Authorization: `Bearer ${token}`
+  },
+});
 
 export const apiCoin = axios.create({
   baseURL: 'https://api.coingecko.com/api/v3/'
